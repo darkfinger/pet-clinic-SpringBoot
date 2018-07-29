beeffeaf3fbd0d9aba8be311128a4c5d45b033f5
darkfinger <892810dkcbak@G>
2018-07-29 12:41:00 -0400

    initial add

---
+++ src/main/java/cpm/dkkcorp/petclinicspringboot/PetClinicSpringbootApplication.java
@@ -0,0 +1,12 @@
+package cpm.dkkcorp.petclinicspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringbootApplication.class, args);
+    }
+}
