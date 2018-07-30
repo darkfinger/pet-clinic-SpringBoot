c9ec533b5734bf5e0729de07ce922337e66b865b
darkfinger <892810dkcbak@G>
2018-07-30 18:34:36 -0400

    refactor interface services to common service. closes #11

--- pet-clinic-data/src/main/java/services/OwnerService.java
+++ pet-clinic-data/src/main/java/services/OwnerService.java
@@ -2,11 +2,7 @@ package services;
 
 import model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long> {
     Owner findByLastName(String name);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
