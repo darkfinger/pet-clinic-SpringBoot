b4e419ee2291e04ac87bfba1c191b8c749bd8fd1
darkfinger <892810dkcbak@G>
2018-07-30 23:42:46 -0400

    adding owner index page to controller. closes #13

--- pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/controller/OwnerController.java
@@ -1,7 +1,13 @@
 package cpm.dkkcorp.petclinicspringboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+
+    @RequestMapping({"","/owners","/owners/index","/owners/index.html","owners/index.php"})
+    public String listOfVet(){
+        return "owners/index";
+    }
 }
