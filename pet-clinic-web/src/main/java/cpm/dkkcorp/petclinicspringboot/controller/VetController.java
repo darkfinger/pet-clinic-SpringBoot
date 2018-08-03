f5ef22bcf27693a0fe1d388a65d1a2035e59e43c
darkfinger <892810dkcbak@G>
2018-08-03 16:13:53 -0400

    show list of vets. closes #15

--- pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/controller/VetController.java
+++ pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/controller/VetController.java
@@ -1,13 +1,23 @@
 package cpm.dkkcorp.petclinicspringboot.controller;
 
+import cpm.dkkcorp.petclinicspringboot.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+    @Autowired
+    private final VetService vetService;
+
     @RequestMapping({"","/vets","/vets/index","/vets/index.html","vets/index.php"})
-    public String listOfVet(){
+    public String listOfVet(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
