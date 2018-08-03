67a605cf1057ffca3c07cf19c87a113a8526eff6
darkfinger <892810dkcbak@G>
2018-08-03 16:38:58 -0400

    id generator implementation. closes #23

--- pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/bootstrap/DateLoader.java
+++ pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/bootstrap/DateLoader.java
@@ -25,31 +25,26 @@ public class DateLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner o1=new Owner();
-        o1.setId(1L);
         o1.setFirstName("david");
         o1.setLastName("kaps");
         ownerService.save(o1);
 
         Owner o2=new Owner();
-        o2.setId(2L);
         o2.setFirstName("glo");
         o2.setLastName("Muks");
         ownerService.save(o2);
 
         Owner o3=new Owner();
-        o3.setId(3L);
         o3.setFirstName("zak");
         o3.setLastName("Mpoyi");
         ownerService.save(o3);
 
         Vet v1=new Vet();
-        v1.setId(1L);
         v1.setFirstName("jean");
         v1.setLastName("kiko");
         vetService.save(v1);
 
         Vet v2=new Vet();
-        v2.setId(2L);
         v2.setFirstName("kitana");
         v2.setLastName("nikal");
         vetService.save(v2);
