285e10f1d4ccbed80134fc71bb8059e49e039ba4
darkfinger <892810dkcbak@G>
2018-08-16 06:36:31 -0400

    enhance owner. closes #33

--- pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/bootstrap/DateLoader.java
+++ pet-clinic-web/src/main/java/cpm/dkkcorp/petclinicspringboot/bootstrap/DateLoader.java
@@ -2,6 +2,7 @@ package cpm.dkkcorp.petclinicspringboot.bootstrap;
 
 
 import cpm.dkkcorp.petclinicspringboot.model.Owner;
+import cpm.dkkcorp.petclinicspringboot.model.Pet;
 import cpm.dkkcorp.petclinicspringboot.model.PetType;
 import cpm.dkkcorp.petclinicspringboot.model.Vet;
 import cpm.dkkcorp.petclinicspringboot.services.OwnerService;
@@ -10,6 +11,8 @@ import cpm.dkkcorp.petclinicspringboot.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DateLoader implements CommandLineRunner {
@@ -41,16 +44,25 @@ public class DateLoader implements CommandLineRunner {
         Owner o1=new Owner();
         o1.setFirstName("david");
         o1.setLastName("kaps");
+        o1.setAddress("palais");
+        o1.setCity("kin");
+        o1.setTelephon("123456");
         ownerService.save(o1);
 
         Owner o2=new Owner();
         o2.setFirstName("glo");
         o2.setLastName("Muks");
+        o2.setAddress("palais");
+        o2.setCity("kin");
+        o2.setTelephon("123456");
         ownerService.save(o2);
 
         Owner o3=new Owner();
         o3.setFirstName("zak");
         o3.setLastName("Mpoyi");
+        o3.setAddress("palais");
+        o3.setCity("kin");
+        o3.setTelephon("123456");
         ownerService.save(o3);
 
         Vet v1=new Vet();
@@ -63,5 +75,26 @@ public class DateLoader implements CommandLineRunner {
         v2.setLastName("nikal");
         vetService.save(v2);
 
+
+        Pet pet1=new Pet();
+        pet1.setName("rosco");
+        pet1.setPetType(savedDog);
+        pet1.setOwner(o1);
+        o1.getPets().add(pet1);
+        pet1.setBirthDate(LocalDate.now());
+
+        Pet pet2=new Pet();
+        pet2.setName("kitty");
+        pet2.setPetType(savedCat);
+        pet2.setOwner(o2);
+        o2.getPets().add(pet2);
+        pet2.setBirthDate(LocalDate.now());
+
+        Pet pet3=new Pet();
+        pet3.setName("doggy");
+        pet3.setPetType(savedDog);
+        pet3.setOwner(o3);
+        o3.getPets().add(pet3);
+        pet3.setBirthDate(LocalDate.now());
     }
 }
