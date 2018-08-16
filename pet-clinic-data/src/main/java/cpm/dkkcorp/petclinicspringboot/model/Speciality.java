6dbbb8ae7c133fe4961dd94e92de5a50595f6537
darkfinger <892810dkcbak@G>
2018-08-15 22:00:36 -0400

    added speciality. closes #27

---
+++ pet-clinic-data/src/main/java/cpm/dkkcorp/petclinicspringboot/model/Speciality.java
@@ -0,0 +1,14 @@
+package cpm.dkkcorp.petclinicspringboot.model;
+
+public class Speciality extends BaseEntity {
+
+    private String descrition;
+
+    public String getDescrition() {
+        return descrition;
+    }
+
+    public void setDescrition(String descrition) {
+        this.descrition = descrition;
+    }
+}
