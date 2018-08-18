952c9905c0ff912724a7182270419b0a14226008
darkfinger <892810dkcbak@G>
2018-08-18 11:37:19 -0400

    jpa severvice for pet entity. closes #43

--- pet-clinic-data/src/main/java/cpm/dkkcorp/petclinicspringboot/services/dataJPA/PetDataJPA.java
+++ pet-clinic-data/src/main/java/cpm/dkkcorp/petclinicspringboot/services/dataJPA/PetDataJPA.java
@@ -1,34 +1,55 @@
 package cpm.dkkcorp.petclinicspringboot.services.dataJPA;
 
 import cpm.dkkcorp.petclinicspringboot.model.Pet;
+import cpm.dkkcorp.petclinicspringboot.repository.PetRepository;
 import cpm.dkkcorp.petclinicspringboot.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
+
+@Service
+@Profile("dataJPA")
 public class PetDataJPA implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetDataJPA(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
 
     @Override
     public Set<Pet> findAll() {
-        return null;
+        Set<Pet> pets=new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
     }
 
     @Override
     public Pet findById(Long aLong) {
-        return null;
+        Optional<Pet> optionalPet= petRepository.findById(aLong);
+        if(optionalPet.isPresent()){
+            return optionalPet.get();
+        }else {
+            return null;
+        }
     }
 
     @Override
     public Pet save(Pet object) {
-        return null;
+        return petRepository.save(object);
     }
 
     @Override
     public void delete(Pet object) {
-
+        petRepository.delete(object);
     }
 
     @Override
     public void deleteById(Long aLong) {
-
+        petRepository.deleteById(aLong);
     }
 }
